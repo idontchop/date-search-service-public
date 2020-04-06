49027002d2f99d5d8499f91151b2b02d89cfa4c3
Nate D <nate@idontchop.com>
2020-04-05 19:50:22 -0700

    Initial Commit

---
+++ src/main/java/com/idontchop/datesearchservice/DateSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.idontchop.datesearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateSearchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idontchop/datesearchservice/DateSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idontchop.datesearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
